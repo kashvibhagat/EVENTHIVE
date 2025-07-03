a523b916037f287751e0406cb2f653ddf4480ef5
kashvibhagat <169167512+kashvibhagat@users.noreply.github.com>
2025-07-03 15:34:53 +0530

    Add files via upload

---
+++ routes.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+// ... other imports
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* add other routes */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
